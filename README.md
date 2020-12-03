3baddbbde78fd5add34bf103d9f19475942710e2
Jeffery Osei <jefferyosei101@gmail.com>
2020-12-03 23:45:19 +0000

    finalize on training assets

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
-# newman training
+# Newman Training
 
 For console lovers who want to automate their postman tests
 
 - Postman Collection: [https://documenter.getpostman.com/view/323124/TVmMgxMP](https://documenter.getpostman.com/view/323124/TVmMgxMP)
+
+### 🐱‍💻 Resources:
+
+Kindly find below links to resources you can use as a guide and also links to my social media should you need to make contact for any challenges you might have or just to have a short conversation if you are starting out.
+
+Postman: https://www.postman.com
+Postman Public APIs: https://www.postman.com/explore
+Postman Learning: https://learning.postman.com/docs/getting-started/introduction
+Postman Newman: https://learning.postman.com/docs/running-collections/using-newman-cli/command-line-integration-with-newman/
+
+GitHub repository for API code:
+https://github.com/clonne101/newman-training
+
+Social Links:
+Website: http://jefferyclonne.com
+Twitter: https://twitter.com/@clonne101
+LinkedIn: https://www.linkedin.com/in/jeffery-osei-551626a6
--- postman.js
+++ postman.js
@@ -1,40 +1,44 @@
-const fs = require('fs'); // require node file system
-const request = require('request'); // require node js request
-const newman = require('newman'); // require newman in your project
-const postman_collection_url = 'https://raw.githubusercontent.com/clonne101/newman-training/main/space_x.json';
-const postman_collection_name = 'space_x.json';
+const fs = require("fs"); // require node file system
+const rimraf = require("rimraf"); // require rm package
+const request = require("request"); // require node js request
+const newman = require("newman"); // require newman
+const postman_collection_url =
+  "https://raw.githubusercontent.com/clonne101/newman-training/main/space_x.json";
+const postman_collection_name = "space_x.json";
 
-
+// method responsible for obtaining our collection
 function getCollectionFile(postman_collection_url, postman_collection_name) {
   return new Promise((resolve, reject) => {
     // check if postman collection json exist, if not download it
-    fs.readFile('./' + postman_collection_name, function(err) {
-      if(err) {
+    fs.readFile("./" + postman_collection_name, function (err) {
+      if (err) {
         // write to log
-        console.log(postman_collection_name + " collection file not found, downloading...");
+        console.log(
+          postman_collection_name + " collection file not found, downloading..."
+        );
 
         // get the file contents
         request(postman_collection_url, function (error, response, body) {
           if (!error && response.statusCode == 200) {
             // write to log
             console.log("Retrieved file successfully, saving...");
-            
+
             // write to file
             fs.writeFile(postman_collection_name, body, function (fail) {
               if (fail) {
                 console.log(fail);
                 reject(fail);
-              };
+              }
 
               // write to log
-              console.log(postman_collection_name + ' saved successfully!');
+              console.log(postman_collection_name + " saved successfully!");
               resolve(true);
             });
           } else {
             console.log(error);
             reject(error);
           }
-        })
+        });
       } else {
         // write to log
         console.log(postman_collection_name + " exist proceeding...");
@@ -44,23 +48,34 @@ function getCollectionFile(postman_collection_url, postman_collection_name) {
   });
 }
 
+// promise declaration
 const promises = [
-  getCollectionFile(postman_collection_url, postman_collection_name)
+  getCollectionFile(postman_collection_url, postman_collection_name),
 ];
 
-
-Promise.all(promises).then(result => {
-  if(result){
+// promise resolver
+Promise.all(promises).then((result) => {
+  if (result) {
     // add space
-    console.log('\n');
-    
-    // call newman.run to pass `options` object and wait for callback
-    newman.run({
-      collection: require('./' + postman_collection_name),
-      reporters: 'cli'
-    }, function (err) {
-      if (err) { throw err; }
-      console.log('\nCollection run complete!\n');
+    console.log("\n");
+
+    // remove reporter folder
+    rimraf("./newman", function () {
+      console.log("Old newman reporters removed successfully...");
     });
+
+    // call newman.run to pass `options` object and wait for callback
+    newman.run(
+      {
+        collection: require("./" + postman_collection_name),
+        reporters: ["html", "csv", "json"],
+      },
+      function (err) {
+        if (err) {
+          throw err;
+        }
+        console.log("\nCollection run complete!\n");
+      }
+    );
   }
 });
